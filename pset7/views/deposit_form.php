d2a26b9d062cef37765416852bfaa6105c115d99
fatboy00 <fatboy00@users.noreply.github.com>
2016-10-01 14:55:55 +0100

    Create deposit_form.php

---
+++ pset7/views/deposit_form.php
@@ -0,0 +1,12 @@
+<form action="deposit.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="amount" placeholder="$0.00" type="text"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                PRESS TO DEPOSIT!
+            </button>
+        </div>
+    </fieldset>
+</form>
