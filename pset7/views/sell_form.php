0926de0e55fab7684a4ff90ddaae2c252bd7077b
fatboy00 <fatboy00@users.noreply.github.com>
2016-10-01 14:58:06 +0100

    Create sell_form.php

---
+++ pset7/views/sell_form.php
@@ -0,0 +1,12 @@
+<form action="sell.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="symbol" placeholder="Stock Symbol to Sell" type="text"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                PRESS TO SELL!
+            </button>
+        </div>
+    </fieldset>
+</form>
