081a4c8434aadbd2a3490e0155d3cc48979e76dc
fatboy00 <fatboy00@users.noreply.github.com>
2016-10-01 14:55:32 +0100

    Create buy_form.php

---
+++ pset7/views/buy_form.php
@@ -0,0 +1,15 @@
+<form action="buy.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="symbol" placeholder="Stock Symbol to Buy" type="text"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="number" placeholder="No. Shares" type="text"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                PRESS TO BUY!
+            </button>
+        </div>
+    </fieldset>
+</form>
